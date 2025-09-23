912437ff442811648542cd975d8a34d17b71f398
Rakibul-98 <rakibul.rupom2001@gmail.com>
2025-09-23 09:20:27 +0600

    make the structure for the body of the landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,23 @@
+import Feed from "../components/home/feed/Feed";
 import Header from "../components/home/header/Header";
+import LeftBar from "../components/home/leftbar/LeftBar";
+import RightBar from "../components/home/rightbar/RightBar";
 
 export default function Home() {
   return (
     <div>
       <Header />
+      <main className="h-screen grid grid-cols-4">
+        <div className="bg-gray-400">
+          <LeftBar />
+        </div>
+        <div className="col-span-2 bg-red-300">
+          <Feed />
+        </div>
+        <div className="bg-blue-100">
+          <RightBar />
+        </div>
+      </main>
     </div>
   );
 }
